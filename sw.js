8dae4e9768f653ebde43ebd24d7e267629ebb19f
Akshay <akshayjs1998@gmail.com>
2026-07-28 12:25:31 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,18 @@
-const CACHE = 'lyffin-ops-v1785216022';
+const CACHE = 'lyffin-ops-v1785221297';
 const FILES = [
-  '/login.html',
-  '/dashboard.html',
-  '/production.html',
-  '/qc.html',
-  '/admin.html',
-  '/capacity.html',
-  '/project.html',
-  '/account.html',
-  '/share.html',
-  '/shared.js',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
+  'login.html',
+  'dashboard.html',
+  'production.html',
+  'qc.html',
+  'admin.html',
+  'capacity.html',
+  'project.html',
+  'account.html',
+  'share.html',
+  'shared.js',
+  'manifest.json',
+  'icon-192.png',
+  'icon-512.png'
 ];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
